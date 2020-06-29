c4016b47d52dad8a27e891302b89c3075aadf447
sai70977venu <tnavneeth5555@gmail.com>
2020-06-29 21:07:54 +0530

    3 update

--- mainserver.js
+++ mainserver.js
@@ -6,14 +6,14 @@ const usermodel = require("./models/usermodel");
 var bodyParser = require('body-parser');
 const auth = require("./routes/auth");//authentication router
 var app = express();
+const morgan= require('winston')
 app.use(bodyParser.json());
 app.use("/auth/",auth);
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 //running on the port
 app.listen(1234,()=>{
     console.log("server connected");
 })
-
 //Set up default mongoose connection
 var mongoDB = 'mongodb+srv://navneeth2000:Navneeth2000@cluster0-tlpr0.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true ,useCreateIndex: true, useUnifiedTopology: true });
